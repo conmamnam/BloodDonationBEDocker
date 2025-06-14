0d425badbb669c1dc070c42a2b52e3963011b164
conmamnam <95952434+conmamnam@users.noreply.github.com>
2025-06-14 23:22:15 +0700

    Delete BloodDonationBEDocker directory

--- BloodDonationBEDocker/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserLoginAPIView, VerifyOTPByEmailAPIView, UserResetPasswordAPIView, RegisterAccountAPIView
-
-urlpatterns = [
-    path('users/login/', UserLoginAPIView.as_view()),
-    path('users/signup/', RegisterAccountAPIView.as_view()),
-    path('verify/send-otp/', VerifyOTPByEmailAPIView.as_view()),
-    path('verify/reset-password/', UserResetPasswordAPIView.as_view()),
-]
